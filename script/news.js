860eb2f90a11eda87f38ceaf91a0fed30e68e6ae
xuedesign <elmo@xuedesign.tw>
2018-07-09 21:47:11 +0800

    內頁編排70% #20180709 關於明華完成 最新消息完成 客戶服務完成

---
+++ script/news.js
@@ -0,0 +1,41 @@
+$LAB
+
+//jQuery總碼
+.script("https://code.jquery.com/jquery.min.js")
+
+//圖片延遲載入
+.script("https://cdn.jsdelivr.net/npm/lazyload@1.8.4/jquery.lazyload.min.js").wait(function(){
+    $("img.lazyload").lazyload();
+  })
+
+//區塊載入動畫
+.script("https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min.js").wait(function(){
+    wow=new WOW({animateClass:"animated",offset:100,callback:function(a){console.log("WOW: animating <"+a.tagName.toLowerCase()+">")}}),wow.init();
+  })
+
+//分享按鈕
+.script("https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b435b020c2ca563")
+
+//totop
+.script("script/lib/jquery.ui.totop.js")
+.script("script/lib/easing.js").wait(function(){
+    $(document).ready(function(){$().UItoTop({easingType:"easeOutQuart"})}); 
+})
+
+//行動版選單
+.script("script/lib/overlay.modernizr.custom.js")
+.script("script/lib/overlay-classie.js")
+.script("script/lib/overlay-set.js")
+
+// 另開視窗
+function externalLinks() { 
+ if (!document.getElementsByTagName) return; 
+ var anchors = document.getElementsByTagName("a"); 
+ for (var i=0; i<anchors.length; i++) { 
+   var anchor = anchors[i]; 
+   if (anchor.getAttribute("href") && 
+       anchor.getAttribute("rel") == "external") 
+     anchor.target = "_blank"; 
+ } 
+} 
+window.onload = externalLinks;
