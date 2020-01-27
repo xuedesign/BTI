f5d5a78ca3725d260d4f51f483fd23e16fe557cc
xuedesign <elmo@xuedesign.tw>
2020-01-27 12:58:27 +0800

    安裝 pop-up 效果 #20200127

---
+++ script/lib/adpopup-setting.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  var ads = {
+    'ad1': {
+      'type': 'image',
+      'src': '../demo/images/pic1.png',
+      'link': 'https://www.facebook.com/%E6%98%8E%E8%8F%AF%E7%94%9F%E6%8A%80-Healthy-body-Happy-life-280940597295/'
+    },
+    // 'ad2': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic2.png',
+    //   'link': 'http://rikdevos.com'
+    // },
+    // 'ad3': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic3.png',
+    //   'link': 'http://rikdevos.com'
+    // },
+    // 'ad4': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic4.png',
+    //   'link': 'http://rikdevos.com'
+    // },
+    // 'ad5': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic5.png',
+    //   'link': 'http://rikdevos.com'
+    // },
+    // 'ad6': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic6.png',
+    //   'link': 'http://rikdevos.com'
+    // },
+    // 'ad7': {
+    //   'type': 'image',
+    //   'src': '../demo/images/pic7.png',
+    //   'link': 'http://rikdevos.com'
+    // }
+
+  }
+
+	  $('body').adPopupPro({
+	    ads: ads,
+	    width: 600,
+      height: 450,
+      min_screen_size: '200x150',
+	  });
+  });
