25b3919910806045471d950c5318a196fa37ed3f
xuedesign <elmo@xuedesign.tw>
2020-10-28 16:09:49 +0800

    更新彈跳視窗廣告 #20201028

--- script/lib/adpopup-setting.js
+++ script/lib/adpopup-setting.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
 
   var ads = {
-    'ad1': {
-      'type': 'image',
-      'src': 'http://www.bti.com.tw/Content/images/popup/abanner_02.jpg',
-      'link': 'https://www.facebook.com/%E6%98%8E%E8%8F%AF%E7%94%9F%E6%8A%80-Healthy-body-Happy-life-280940597295/'
-    },
-    // 'ad2': {
-    //   'type': 'image',
-    //   'src': '../demo/images/pic2.png',
-    //   'link': 'http://rikdevos.com'
+    //'ad1': {
+    //  'type': 'image',
+    //  'src': 'http://www.bti.com.tw/Content/images/popup/abanner_02.jpg',
+    //  'link': 'https://www.facebook.com/%E6%98%8E%E8%8F%AF%E7%94%9F%E6%8A%80-Healthy-body-Happy-life-280940597295/'
+    //},
+    // 'ad1': {
+    //   'type': 'iframe',
+    //   'src': 'http://www.bti.com.tw/Uploads/VIDEO/bti系列商品_影片.mp4'
     // },
-    // 'ad3': {
-    //   'type': 'image',
-    //   'src': '../demo/images/pic3.png',
-    //   'link': 'http://rikdevos.com'
-    // },
+     'ad3': {
+       'type': 'image',
+       'src': 'http://www.bti.com.tw/Content/images/popup/abanner-20201028.gif',
+       'link': 'https://www.facebook.com/%E6%98%8E%E8%8F%AF%E7%94%9F%E6%8A%80-Healthy-body-Happy-life-280940597295/'
+     },
     // 'ad4': {
     //   'type': 'image',
     //   'src': '../demo/images/pic4.png',
@@ -38,12 +37,13 @@ $(document).ready(function() {
     // }
 
   }
-
-	  $('body').adPopupPro({
+ 
+    $('body').adPopupPro({
       ads: ads,
-      show_type: 'session',
+      show_type: 'always',
 	    width: 600,
       height: 450,
       min_screen_size: '650x500',
+      overlay_color: 'dark'
 	  });
   });
